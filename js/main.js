602f24ce1f6821454c78092c72735e4f1d6ab4c0
EroyEroy <vladislavtersenev7@gmail.com>
2022-10-13 08:35:11 +0300

    resize

--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
 window.addEventListener('load', () => {
-	const media = document.querySelectorAll('a, img');
+	const media = document.querySelectorAll('a, img'),
+		allImg = document.querySelectorAll('img');
 	for (let i = 0; i < media.length; i++) {
 		media[i].setAttribute('draggable', false);
+	};
+	for (let i = 0; i < allImg.length; i++) {
+		allImg[i].setAttribute('loading', 'lazy');
 	};
 });
